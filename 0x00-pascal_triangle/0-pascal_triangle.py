5e3145e5acaa4ce1f2313632514f6e163a98c982
Hoodahashem <mahmoudhashem335@gmail.com>
2024-04-27 03:51:10 +0300

    finish

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,27 @@
+def pascal_triangle(n):
+    """Returns a list of lists of integers representing the Pascal’s triangle of n."""
+
+    if n <= 0:
+        return []
+
+    RLIST = []
+    lst = []
+    for i in range(n):
+        if len(lst) == 0:
+            lst.append(1)
+            RLIST.append([1])
+        elif len(lst) == 1:
+            lst.append(1)
+            RLIST.append([1,1])
+        else:
+            ndlst = []
+            x = 0
+            while x < len(lst):
+                if x + 1 != len(lst):
+                    ndlst.append(lst[x] + lst[x + 1])
+                x += 1
+            lst = [1]
+            lst.extend(ndlst)
+            lst.append(1)
+            RLIST.append(lst)
+    return RLIST
